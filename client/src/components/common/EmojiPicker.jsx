3d895ff528581b442a138f3ac5542720169c5134
seiryu23 <64452921+seiryu23@users.noreply.github.com>
2023-06-06 00:50:42 +0900

    2023.6.6 Chg

---
+++ client/src/components/common/EmojiPicker.jsx
@@ -0,0 +1,20 @@
+import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+
+const EmojiPicker = (props) => {
+    const [selectedEmoji, setSelectedEmoji] = useState();
+
+    useEffect(() => {
+        setSelectedEmoji(props.icon);
+    }, [props.icon]);
+
+    return (
+        <Box>
+            <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer"}}>
+                {selectedEmoji}
+            </Typography>
+        </Box>
+    );
+};
+
+export default EmojiPicker
